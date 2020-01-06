3c58a012a6dce5db0a8e4f749c75204bc4dbd665
hellomoonclown <59511158+hellomoonclown@users.noreply.github.com>
2020-01-06 17:11:07 -0500

    Add files via upload

--- FLWclient/src/flwclient/FLWclient.java
+++ FLWclient/src/flwclient/FLWclient.java
@@ -5,6 +5,9 @@
  */
 package flwclient;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 import javax.swing.JFrame;
 
 /**
@@ -16,13 +19,25 @@ public class FLWclient {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         JFrame frame = new JFrame("FLW wallet");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 300);
         frame.setVisible(true);
         frame.setLayout(null);
+        
+        // connect to server
+        try {
+         InetAddress addr;
+         Socket sock = new Socket("localhost", 3333);
+         addr = sock.getInetAddress();
+         System.out.println("Connected to " + addr);
+         sock.close();
+      } catch (java.io.IOException e) {
+         System.out.println("Can't connect to " + args[0]);
+         System.out.println(e);
+      }
         
     }
     
