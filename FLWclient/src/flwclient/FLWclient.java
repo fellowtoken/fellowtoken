5dfdb241050cd3f33ccffa1e905c825907afab45
hellomoonclown <59511158+hellomoonclown@users.noreply.github.com>
2020-01-06 16:06:22 -0500

    Added source for the wallet/client

---
+++ FLWclient/src/flwclient/FLWclient.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flwclient;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author n3t
+ */
+public class FLWclient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame frame = new JFrame("FLW wallet");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 300);
+        frame.setVisible(true);
+        frame.setLayout(null);
+        
+    }
+    
+}
